e0bd22f097740dca9ae562eaa90bd4fc4d162468
liupishui <liupishui@163.com>
2019-05-27 14:22:26 +0800

    Delete erzhi2direction.js

--- app/example/secret/erzhi2direction.js
+++
@@ -1,7 +0,0 @@
-let ocrcheckcode = require('../main');
-let fs = require('fs');
-let DirectionArr = ocrcheckcode.erzhi2direction(__dirname +'/jsonStudy.txt');
-for(let x in DirectionArr){
-    console.log(x)
-}
-fs.writeFileSync(__dirname + '/jsonStudyDirection.txt', JSON.stringify(DirectionArr));
